d0c9b9dc217b71f6ac44574c52a8cd2440e1b9b9
IgorThiagoLara <163324764+IgorThiagoLara@users.noreply.github.com>
2024-03-20 10:50:49 -0300

    Update teste2.py

--- teste2.py
+++ teste2.py
@@ -8,16 +8,14 @@ x1 = (-b + delta)/2*a
 x2 = (-b - delta)/2*a
 
 if delta >=1:
-    print("Existem duas raízes distintas")
+    print(f"Como delta é {delta}, existem duas raízes distintas")
     print(f"o resultado 1 é: {x1}")
     print(f"o resultado 2 é: {x2}")
 elif delta ==0:
-    print("Existem duas raízes reais iguais")
+    print(f"Como delta é {delta}, existem duas raízes reais iguais")
     print(f"o resultado 1 é: {x1}")
     print(f"o resultado 2 é: {x2}")
 elif delta <0:
-    print("Existem duas raízes imaginárias")
+    print(f"Como delta é {delta}, existem duas raízes imaginárias")
     print(f"o resultado 1 é: {x1}")
     print(f"o resultado 2 é: {x2}")
-else:
-    print("Erro")
